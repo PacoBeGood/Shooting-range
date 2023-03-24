b26e07836f84e9441722a9f5bd3207354265745f
gaming <gaming@t2-campus.eu>
2023-03-24 14:49:29 +0100

    shooting mechanism added

--- Assets/Scripts/Shootingscript.cs
+++ Assets/Scripts/Shootingscript.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class Shootingscript : MonoBehaviour
 {
 
-    [SerializeField] GameObject bullet;
-    [SerializeField] GameObject bullet1;
+    [SerializeField] GameObject gun;
+     GameObject bullet1;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,14 +16,15 @@ public class Shootingscript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+         bullet1.transform.position = bullet1.transform.position += new Vector3(0,0,1);
     }
 
-    void Shoot()
+    public void Shoot()
     {
-        bullet1.transform.position = gameObject.transform.position;
-        bullet1 = Instantiate(bullet);
-        bullet1.transform.position = bullet1.transform.position += new Vector3(0,0,1);
+        bullet1 = Instantiate(gameObject);
+        bullet1.transform.position = gun.transform.position;
+        
+       
     }
 
 }
