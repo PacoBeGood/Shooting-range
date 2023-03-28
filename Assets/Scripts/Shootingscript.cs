749693acc7aa0bd8dd5ec0e4335de9ff37c89c6f
Yentl Sorgi <Yentlsorgi@gmail.com>
2023-03-28 12:18:36 +0200

    fixed firing, gun is now pointed forward

--- Assets/Scripts/Shootingscript.cs
+++ Assets/Scripts/Shootingscript.cs
@@ -17,16 +17,20 @@ public class Shootingscript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+
+        
          
     }
+
+
 
     public void Shoot()
     {
         bullet1 = Instantiate(gameObject);
         bulletSpawn.GetComponent<Collider>();
         bullet1.transform.position = bulletSpawn.position;
-        bullet1.GetComponent<Rigidbody>().AddForce(bulletSpawn.forward * 20, ForceMode.Impulse);
+        bullet1.GetComponent<Rigidbody>().AddForce(bulletSpawn.forward * -20, ForceMode.Impulse);
+        Physics.IgnoreCollision(gun.GetComponent<Collider>(), bullet1.GetComponent<Collider>());
         
        
     }
